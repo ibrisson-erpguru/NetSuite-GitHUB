0f40be6ec37084f395c45a34fc075ce9fd813f12
Ianic Brisson <ianic.briss@erpguru.com>
2014-03-04 20:39:46 -0500

    Initial folder

---
+++ TMP/Root/Client.js
@@ -0,0 +1,18 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       04 Mar 2014     brisson
+ *
+ */
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType 
+ * 
+ * @param {String} type Access mode: create, copy, edit
+ * @returns {Void}
+ */
+function clientPageInit(type){
+   
+}
